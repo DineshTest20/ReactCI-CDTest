483f5d974623a1e1c9426286b480e01c4567e807
Dineshkumar <dineshkumar.g@admindroid.com>
2025-11-12 19:32:55 +0530

    updated the vite.config.js

--- my-react-app/vite.config.js
+++ my-react-app/vite.config.js
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// vite.config.js
 
-// https://vite.dev/config/
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+// Use the name of your GitHub repository as the base path
+const repoName = 'ReactCI-CDTest'; 
+
 export default defineConfig({
   plugins: [react()],
-})
+  
+  // 💥 ADD OR UPDATE THIS LINE 💥
+  base: `/${repoName}/`, 
+  // or simply base: '/ReactCI-CDTest/',
+});
